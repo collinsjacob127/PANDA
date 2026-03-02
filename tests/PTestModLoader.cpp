fb198887b47faa39b28542b052185263a24f8588
Eric Galvan <lericgalvan@outlook.com>
2026-03-01 18:43:17 -0800

    Remove unnecessary stubs for now

--- tests/PTestModLoader.cpp
+++ tests/PTestModLoader.cpp
@@ -7,35 +7,7 @@ class PTestModLoader : public QObject
 {
     Q_OBJECT
 private slots:
-    void testGetIconPngPaths_data();
-    void testGetIconPngPaths();
 };
-
-// Statics
-QString testDataDir = QFINDTESTDATA("test_data/");
-
-void PTestModLoader::testGetIconPngPaths_data()
-{
-    QTest::addColumn<QStringList>("filePathList");
-    QTest::addColumn<bool>("expected");
-
-    QTest::newRow("list of icon paths") << testDataDir + "config.ini" << true;
-}
-
-void PTestModLoader::testGetIconPngPaths()
-{
-    QFETCH(QString, filePath);
-    QFETCH(bool, expected);
-
-    PConfigMgr configMgr;
-    bool result = configMgr.loadConfig(filePath);
-
-    if (expected) {
-        QVERIFY(result);
-    } else {
-        QVERIFY(!result);
-    }
-}
 
 QTEST_MAIN(PTestModLoader)
 #include "PTestModLoader.moc"
