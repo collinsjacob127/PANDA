1908cfdf4a6b60b3cd1dd39e70119053b5b29157
Eric Galvan <lericgalvan@outlook.com>
2026-03-01 18:12:36 -0800

    Updated CMakeLists with correct pathing; remove declared functions from PTestModLoader.cpp

--- tests/PTestModLoader.cpp
+++ tests/PTestModLoader.cpp
@@ -7,37 +7,8 @@ class PTestModLoader : public QObject
 {
     Q_OBJECT
 private slots:
-    void testGetZtdList_data();
-    void testGetZtdList();
-    void testLoadModsFromFile_data();
-    void testLoadModsFromFile();
-    void testDeleteIcons_data();
-    void testDeleteIcons();
-    void testBuildCollectionMods_data();
-    void testBuildCollectionMods();
-    void testBuildModFromToml_data();
-    void testBuildModFromToml();
-    void testBuildDefaultMod_data();
-    void testBuildDefaultMod();
-    void testGenerateFileData_data();
-    void testGenerateFileData();
-    void testGenerateTagsFromConfig_data();
-    void testGenerateTagsFromConfig();
-    void testDetermineCategory_data();
-    void testDetermineCategory();
     void testGetIconPngPaths_data();
     void testGetIconPngPaths();
-
-    void testGetIconAniPaths_data();
-    void testGetIconAniPaths();
-    void testGetIconPaths_data();
-    void testGetIconPaths();
-    void testBuildGraphicPath_data();
-    void testBuildGraphicPath();
-    void testDetermineDescription_data();
-    void testDetermineDescription();
-    void testDetermineTitle_data();
-    void testDetermineTitle();
 };
 
 // Statics
