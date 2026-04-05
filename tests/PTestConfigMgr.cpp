53bd2003c310bb93422e5dcf2683b01f32dc09f1
Eric Galvan <lericgalvan@outlook.com>
2026-04-05 09:22:18 -0700

    Reduce line length in unit test

--- tests/PTestConfigMgr.cpp
+++ tests/PTestConfigMgr.cpp
@@ -53,7 +53,9 @@ void PTestConfigMgr::testLoadConfig_tempFileCleanup()
     QStringList beforeFiles = tempDir.entryList(QDir::Files);
     qDebug() << "Before files:" << beforeFiles << "\n";
 
-    const QSharedPointer<PFile> fileData = QSharedPointer<PFile>::create(nullptr, testDataDir + "config.zip", FileType::Zip);
+    const QSharedPointer<PFile> fileData = QSharedPointer<PFile>::create(
+        nullptr, testDataDir + "config.zip", FileType::Zip
+    );
     QSharedPointer<PFileData> fileDataPtr = fileData->read("config.ini");
     PConfigMgr configMgr;
     configMgr.loadConfig(fileDataPtr);
