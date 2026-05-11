8926b14dcd007b3ce6d6ef616a36b6a2213291ba
Jacob Collins <collinsjacob127@gmail.com>
2026-05-11 00:57:39 -0700

    Added missing methods & corrected linkages

--- src/data/PModList.h
+++ src/data/PModList.h
@@ -1,138 +1,77 @@
-#ifndef PMODLIST_H
-#define PMODLIST_H
+#pragma once
 
-#include <QAbstractListModel>
-#include <QSharedPointer>
-#include <QVector>
-#include <QDebug>
-
+#include "PAbstractListModel.h"
 #include "PModItem.h"
 
-class PModList : public QAbstractListModel
+#include <QVector>
+#include <QSharedPointer>
+
+class PModList : public PAbstractListModel
 {
     Q_OBJECT
 
 public:
-    explicit PModList(QObject* parent = nullptr) : QAbstractListModel(parent) {}
-
-    // size of the list
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override
+    explicit PModList(QObject* parent = nullptr)
+        : PAbstractListModel(parent)
     {
-        Q_UNUSED(parent);
-        return m_items.size();
     }
 
-    // return the data for the given index and role
-    QVariant data(const QModelIndex& index, int role) const override
-    {
-        qDebug() << "Fetching data for index:" << index.row() << ", role:" << role;
-        if (!isValidIndex(index)) {
-            // Return empty if the index is invalid
-            qDebug() << "Invalid index:" << index.row() << ", returning empty QVariant";
-            return {};
-        }
-        return dataForRow(index.row(), role);
-    }
-
-    // set the data for the given index and role
-    bool setData(const QModelIndex& index, const QVariant& value, int role) override
-    {
-        if (!isValidIndex(index)) { return false; }
-        bool changed = setDataForRow(index.row(), value, role);
-        if (changed) { emit dataChanged(index, index, { role }); }
-        return changed;
-    }
-
-    // return the role names for the model
     QHash<int, QByteArray> roleNames() const override
     {
         return PModItem::roleNames();
     }
 
-    // add item to the list
+protected:
+    int itemCount() const override
+    {
+        return m_items.size();
+    }
+
+    QVariant dataForRow(
+        int row,
+        int role) const override
+    {
+        return m_items[row]->getData(role);
+    }
+
+    bool setDataForRow(
+        int row,
+        const QVariant& value,
+        int role) override
+    {
+        m_items[row]->setData(role, value);
+        return true;
+    }
+
+public:
     void addItem(QSharedPointer<PModItem> item)
     {
-        beginInsertRows(
-            QModelIndex(),
-            m_items.size(),
-            m_items.size());
-
+        beginInsertRows({}, m_items.size(), m_items.size());
         m_items.append(item);
-
         endInsertRows();
     }
 
-    // remove item from the list
-    void removeItem(int index)
-    {
-        if (index < 0 || index >= m_items.size()) {
-            return;
-        }
-
-        beginRemoveRows(QModelIndex(), index, index);
-
-        m_items.removeAt(index);
-
-        endRemoveRows();
-    }
-
-    // remove multiple items from the list
-    void removeItems(
-        const QVector<QSharedPointer<PModItem>>& items)
-    {
-        beginResetModel();
-
-        for (const auto& item : items) {
-            m_items.removeOne(item);
-        }
-
-        endResetModel();
-    }
-
-    // replace item in the list
-    void replaceItem(
-        int row,
-        QSharedPointer<PModItem> item)
+    void removeItem(int row)
     {
         if (row < 0 || row >= m_items.size()) {
             return;
         }
 
-        beginResetModel();
-
-        m_items[row] = item;
-
-        endResetModel();
+        beginRemoveRows({}, row, row);
+        m_items.removeAt(row);
+        endRemoveRows();
     }
 
-    void replaceList(
-        const QVector<QSharedPointer<PModItem>>& list)
+    void clear()
     {
         beginResetModel();
-
-        m_items = list;
-
-        endResetModel();
-    }
-
-    // Clear the list
-    void clearList()
-    {
-        beginResetModel();
-
         m_items.clear();
-
         endResetModel();
     }
 
     int size() const
     {
-        return m_items.size();
-    }
-
-    int indexOf(QSharedPointer<PModItem> item) const
-    {
-        return m_items.indexOf(item);
+        return m_list.size();
     }
 
     QSharedPointer<PModItem> getItem(int index) const
@@ -143,6 +82,22 @@ public:
         return nullptr; // Return null if index is out of bounds
     }
 
+    void replaceItem(int row, QSharedPointer<PModItem> item)
+    {
+        if (row >= 0 && row < m_list.size()) {
+            beginResetModel();
+            m_list[row] = item;
+            endResetModel();
+        }
+    }
+
+    void replaceList(QVector<QSharedPointer<PModItem>> list)
+    {
+        beginResetModel();
+        m_list = list;
+        endResetModel();
+    }
+
     QModelIndex getIndex(int index) const
     {
         if (index >= 0 && index < m_list.size()) {
@@ -151,32 +106,21 @@ public:
         return QModelIndex(); // Return invalid index if out of bounds
     }
 
-protected:
-    virtual QVariant dataForRow(
-        int row,
-        int role) const
+
+    QSharedPointer<PModItem> itemAt(int row) const
     {
-        return m_items[row]->getData(role);
+        if (row < 0 || row >= m_items.size()) {
+            return {};
+        }
+
+        return m_items[row];
     }
 
-    virtual bool setDataForRow(
-        int row,
-        const QVariant& value,
-        int role)
+    int indexOf(QSharedPointer<PModItem> item) const
     {
-        m_items[row]->setData(role, value);
-        return true;
+        return m_list.indexOf(item);
     }
 
-    bool isValidIndex(const QModelIndex& index) const
-    {
-        return index.isValid()
-            && index.row() >= 0
-            && index.row() < m_items.size();
-    }
-
-protected:
+private:
     QVector<QSharedPointer<PModItem>> m_items;
 };
-
-#endif // PMODLIST_H
